84a6027e8e796948a18df3684c785b7bc10dc873
Vanessa <vannexxaa@gmail.com>
2020-11-04 23:49:18 +0000

    Update test_amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -31,5 +31,49 @@ class Test_Amenity(unittest.TestCase):
         self.assertEqual(type(base.created_at), datetime.datetime)
         self.assertEqual(type(base.updated_at), datetime.datetime)
 
+    def testtype(self):
+        """ Test type class """
+        base = Amenity()
+        self.assertAlmostEqual(type(base), Amenity)
+
+    def test_updated_at(self):
+        """
+            test updated_at
+        """
+        base = Amenity()
+        create = str(base.created_at)
+        start = str(base.updated_at)
+        base.name = "Didier"
+        base.save()
+        self.assertNotEqual(str(base.updated_at), start)
+        self.assertEqual(str(base.created_at), create)
+
+    def test_to_dict(self):
+        """ Tests dict """
+        base = Amenity()
+        base2 = base.to_dict()
+        self.assertEqual(base2["updated_at"], base.updated_at.isoformat())
+        self.assertEqual(base2["__class__"], "Amenity")
+        self.assertNotIn("__class__", base.__dict__)
+
+    def test_save(self):
+        """ Tests save """
+        base = Amenity()
+        base.save()
+        with open("file.json", mode="r", encoding="UTF-8") as f:
+            d = json.load(f)
+        for item in d:
+            if base.id in item:
+                d = d[item]
+        self.assertDictEqual(d, base.to_dict())
+
+    def test_new_model_dict(self):
+        """ Tests new model with dictionary """
+        base = Amenity()
+        dict1 = base.to_dict()
+        base2 = Amenity(**dict1)
+        self.assertFalse(base is base2)
+        self.assertDictEqual(base.to_dict(), base2.to_dict())
+
 if __name__ == '__main__':
     unittest.main()
