b1c9860bc0325709bfca8b447c50caac753677a7
Vanessa <vannexxaa@gmail.com>
2020-11-04 22:58:30 +0000

    Correction idented

--- models/user.py
+++ models/user.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""The module"""
+"""The module User"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """ 
+       Attributes:
+       email: email address
+       password: password for you login
+       first_name: first name
+       last_name: last name
+    """
+
     email = ""
     password = ""
     first_name = ""
