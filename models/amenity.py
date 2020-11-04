298de54135dbb3be738a7d6cbc176493fdfea4e8
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-11-04 16:23:22 -0500

    add comments

--- models/amenity.py
+++ models/amenity.py
@@ -6,5 +6,9 @@ from models.base_model import BaseModel
  class Amenity
  """
 class Amenity(BaseModel):
-   
+    """
+    This is the class for Amenity
+    Attributes:
+        name: input name
+    """
     name = ""
