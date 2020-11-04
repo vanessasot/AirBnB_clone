79dd8ebf7bdf489fe78f105748659362f79d46cd
Jhonnyer Otalvaro <64118164+Jhonierk@users.noreply.github.com>
2020-11-04 16:21:06 -0500

    add comments

--- models/place.py
+++ models/place.py
@@ -6,6 +6,21 @@ from models.base_model import BaseModel
 Class place
 """
 class Place(BaseModel):
+    """
+    This is the class for Place
+    Attributes:
+        city_id: city id
+        user_id: user id
+        name: name input
+        description: string of description
+        number_rooms: number of room in int
+        number_bathrooms: number of bathrooms in int
+        max_guest: maximum guest in int
+        price_by_night:: pice for a staying in int
+        latitude: latitude in flaot
+        longitude: longitude in float
+        amenity_ids: list of Amenity ids
+    """
     
     city_id = ""
     user_id = ""
