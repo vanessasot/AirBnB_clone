5ed158a8f54e3ceb7f0f0a445ca92459f11b66fb
didierrevelo <1829@holbertonschool.com>
2020-11-03 23:25:23 -0500

    tests engine file storage

---
+++ test_file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" tests file storage
+"""
+from os.path import basename
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+import unittest
+from models import storage
+import datetime
+import json
+import os
+
+
+class TestFileStorage(unittest.TestCase):
+    """ init the unnittests
+    """
+    def test_doc(self):
+        """ docstring test """
+        self.assertIsNotNone(("models.engine.file_storage".__doc__))
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.__init__.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
+
+    def test_attr(self):
+        """ tests attr
+        """
+        file = FileStorage()
+        self.assertTrue(hasattr(file, "_FileStorage__objects"))
+        self.assertTrue(hasattr(file, "_FileStorage__file_path"))
+
+    def test_savel(self):
+        """ reload, save test """
+        os.remove('file.json')
+        base = BaseModel()
+        base.save()
+        storage.reload()
+        new = storage.all()
+        self.assertDictEqual(new["BaseModel." + base.id].to_dict(), base.to_dict())
+
+
+if __name__ == '__main__':
+    unittest.main()
