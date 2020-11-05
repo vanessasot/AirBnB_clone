af433093272cb94ddac5dcc11034782d6447afb9
Jhonierk <jorge.otalvaro00@usc.edu.co>
2020-11-05 00:40:09 +0000

    the test user fix add comment

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -12,6 +12,7 @@ from models import storage
 
 
 class Test_user(unittest.TestCase):
+    """this will test the User class"""
 
     def test_doc(self):
         """ Tests docstring """
@@ -34,3 +35,46 @@ class Test_user(unittest.TestCase):
         """ Test type class """
         base = User()
         self.assertAlmostEqual(type(base), User)
+
+    def test_updated_at(self):
+        """
+            test updated_at
+        """
+        base = User()
+        create = str(base.created_at)
+        start = str(base.updated_at)
+        base.name = "Didier"
+        base.save()
+        self.assertNotEqual(str(base.updated_at), start)
+        self.assertEqual(str(base.created_at), create)
+
+    def test_to_dict(self):
+        """ Tests dict """
+        base = User()
+        base2 = base.to_dict()
+        self.assertEqual(base2["updated_at"], base.updated_at.isoformat())
+        self.assertEqual(base2["__class__"], "User")
+        self.assertNotIn("__class__", base.__dict__)
+
+    def test_save(self):
+        """ Tests save """
+        base = User()
+        base.save()
+        with open("file.json", mode="r", encoding="UTF-8") as f:
+            d = json.load(f)
+        for item in d:
+            if base.id in item:
+                d = d[item]
+        self.assertDictEqual(d, base.to_dict())
+
+    def test_new_model_dict(self):
+        """ Tests new model with dictionary """
+        base = User()
+        dict1 = base.to_dict()
+        base2 = User(**dict1)
+        self.assertFalse(base is base2)
+        self.assertDictEqual(base.to_dict(), base2.to_dict())
+
+
+if __name__ == '__main__':
+    unittest.main()
