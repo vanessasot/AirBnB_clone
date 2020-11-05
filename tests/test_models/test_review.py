34bb9badc9c33d89cf861ed71c78c71d20c41998
Vanessa <vannexxaa@gmail.com>
2020-11-05 00:17:57 +0000

    update test_review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -35,3 +35,46 @@ class Test_Review(unittest.TestCase):
         """ Test type class """
         base = Review()
         self.assertAlmostEqual(type(base), Review)
+
+    def test_updated_at(self):
+        """
+            test updated_at
+        """
+        base = Review()
+        create = str(base.created_at)
+        start = str(base.updated_at)
+        base.name = "Didier"
+        base.save()
+        self.assertNotEqual(str(base.updated_at), start)
+        self.assertEqual(str(base.created_at), create)
+
+    def test_to_dict(self):
+        """ Tests dict """
+        base = Review()
+        base2 = base.to_dict()
+        self.assertEqual(base2["updated_at"], base.updated_at.isoformat())
+        self.assertEqual(base2["__class__"], "Review")
+        self.assertNotIn("__class__", base.__dict__)
+
+    def test_save(self):
+        """ Tests save """
+        base = Review()
+        base.save()
+        with open("file.json", mode="r", encoding="UTF-8") as f:
+            d = json.load(f)
+        for item in d:
+            if base.id in item:
+                d = d[item]
+        self.assertDictEqual(d, base.to_dict())
+
+    def test_new_model_dict(self):
+        """ Tests new model with dictionary """
+        base = Review()
+        dict1 = base.to_dict()
+        base2 = Review(**dict1)
+        self.assertFalse(base is base2)
+        self.assertDictEqual(base.to_dict(), base2.to_dict())
+
+
+if __name__ == '__main__':
+    unittest.main()
